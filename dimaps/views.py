876b0416148247e9c764a326d8fd336ea18802d1
Kelvin <kelvin_guerrero@hotmail.com>
2014-10-25 13:33:48 -0500

    Initial commit

---
+++ dimaps/views.py
@@ -0,0 +1,4 @@
+from setuptools.command.sdist import re_finder
+from django.shortcuts import render
+
+# Create your views here.
---
+++ mati/settings.py
@@ -0,0 +1,110 @@
+"""
+Django settings for mati project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'k6&-te(8kpn&oif$4e1_lvfw6dl#fp!(&+=kr)+vdt5srs@ea0'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'bootstrapform',
+    'dimaps',
+    'home',
+    'south',
+
+    #'django_extensions', #python manage.py show_urls
+#    'generate_scaffold',
+)
+
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'mati.urls'
+
+WSGI_APPLICATION = 'mati.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dimap_db',
+        'USER': 'postgres',
+        'PASSWORD': 'kelvin',
+        'HOST': 'localhost',
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+#DATABASES
+#'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/bootstrap'),
+    os.path.join(BASE_DIR, 'static/jquery'),
+    os.path.join(BASE_DIR, 'static/images'),
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'templates'),
+    os.path.join(BASE_DIR,  'templates/dimaps'),
+    os.path.join(BASE_DIR,  'templates/dimaps/service'),
+)
